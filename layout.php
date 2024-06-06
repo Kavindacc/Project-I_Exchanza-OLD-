afbc78d3e3c18d4e2ada52ba63c334115b1a2104
Kavindacc <kavindacc@gmail.com>
2024-06-06 21:47:28 +0530

    Create Biding page first Section

--- layout.php
+++ layout.php
@@ -1,13 +1,13 @@
+{{ %  % }}
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="view/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -18,7 +18,7 @@
     <!--nav bar-->
     <nav class="navbar navbar-expand-lg bg-body-tertiary  sticky-top">
         <div class="container-fluid"><!--logo-->
-            <a class="navbar-brand" href="#"><img src="../img/Exchanza.png" width="100px"></a>
+            <a class="navbar-brand" href="#"><img src="img/Exchanza.png" width="100px"></a>
             <!--toggle button-->
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -67,68 +67,14 @@
     </nav>
 
 
-    <!-- content -->
-    <div class="flex w-full flex-col pb-10 ">
-
-        <div class="flex flex-row w-full bg-[#F3F3F3] px-3">
-            
-            <div class="hidden md:flex w-full justify-center items-center ">
-                <div class="flex flex-col gap-3">
-
-                    <h1 class="text-xl">Classic Exclusive</h1>
-                    <h1 class="text-3xl font-bold " >Women's Collection</h1>
-                    <h1 class="text-md font-medium " >UPTO 40% OFF</h1>
-
-                    <div class="mt-3 w-full">
-                        <button class=" p-2 flex justify-center items-center text-md font-semibold text-white bg-black rounded-md px-3">Shop Now</button>
-                    </div>
-
-                </div>
-            </div>
-            
-            <div class="flex flex-col w-full justify-start ">
-                <img class="w-full " src="../img/bid-main div bg.webp" />
-                <div class="flex text-white md:hidden md:text-white flex-col gap-3 relative top-[-45%] left-[10%]">
-
-                    <h1 class="text-xl">Classic Exclusive</h1>
-                    <h1 class="text-3xl font-bold " >Women's Collection</h1>
-                    <h1 class="text-md font-medium " >UPTO 40% OFF</h1>
-
-                    <div class="mt-3 w-full">
-                        <button class=" p-2 flex justify-center items-center text-md font-semibold text-white bg-black rounded-md px-3">Shop Now</button>
-                    </div>
-
-                </div>
-            </div>
-    
-        </div>
-
-        <div class="flex flex-col px-12 pt-16 bg-red bg-[#F3F3F3]">
-
-            <div class="flex justify-between flex-row w-full bg-[#F3F3F3] px-3">
-                <h1 class='text-2xl text-semibold my-auto'>Shop by Categories</h1>
-
-                <div class="flex gap-3 my-auto">
-                    <button class="p-1 bg-[black] text-white rounded-md px-2"><li class="fa-solid fa-magnifying-glass"""></li></button>
-                    <button class="p-1 rounded-md px-2 bg-[#F6F6F6]"><li class="fa-solid fa-magnifying-glass"""></li></button>
-                </div>
-
-                <div class="flex flex-col w-full""">asd</div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-
+    {{  }}
 
     <!--footer-->
     <div class="container-fluid" style="background-color:burlywood;">
         <div class="container p-3">
             <div class="row">
                 <div class="col">
-                    <img src="../img/Exchanza.png" width="200px">
+                    <img src="img/Exchanza.png" width="200px">
                 </div>
             </div>
             <div class="row  mt-4" style="border-bottom:1px solid black;">
