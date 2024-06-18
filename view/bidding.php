e90a31da82069042dd363f2283497496544b014c
Kavindacc <kavindacc@gmail.com>
2024-06-18 10:57:12 +0530

    Updated with index.php to update Navbar and fotter

--- view/bidding.php
+++ view/bidding.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,28 +6,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>index</title>
 </head>
 
-<body style="background-color: gainsboro;">
+<body>
     <!--nav bar-->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary  sticky-top">
-        <div class="container-fluid"><!--logo-->
+    <nav class="navbar navbar-expand-lg sticky-top nav">
+        <div class="container-fluid logo"><!--logo-->
             <a class="navbar-brand" href="#"><img src="../img/Exchanza.png" width="100px"></a>
             <!--toggle button-->
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!--sidebar-->
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <!--sidebarheader-->
                 <div class="offcanvas-header border-bottom">
                     <h5 class="offcanvas-title " id="offcanvasNavbarLabel">Exchanze</h5>
@@ -48,18 +44,24 @@
                             <a class="nav-link" href="#">Selling</a>
                         </li>
                     </ul>
-                    <form class="d-flex me-4" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search">
-                        <a href="#1" class="nav-link  text-decoration-none  mt-2"><i
-                                class="fa-solid fa-magnifying-glass"></i></a>
+                    <form class="d-flex me-4 align-items-center" role="search">
+                        <input class="search me-2" type="search" placeholder="Search">
+                        <a href="#1" class="nav-link  text-decoration-none  mt-1"><i class="fa-solid fa-magnifying-glass"></i></a>
                     </form>
                     <!--login nav-link-a-color-->
-                    <div
-                        class="d-flex flex-column flex-lg-row float-start  justify-content-center  align-items-center mt-3 mt-lg-0 gap-3">
-                        <a href="#1" class="nav-link  text-decoration-none mx-2"><i
-                                class="fa-solid fa-cart-plus"></i></a>
-                        <button class="btn btn-dark btn-sm ms-2 px-3"><a href="view/login.php"
-                                class="nav-link text-decoration-none ">login</a></button>
+                    <div class="d-flex flex-column float-start flex-lg-row justify-content-center  align-items-center mt-3 mt-lg-0 gap-3">
+                        <a href="../Project-I_Exchanza/view/cart.php" class="nav-link  text-decoration-none mx-1"><i class="fa-solid fa-cart-plus"><span></span></i></a>
+                        <?php
+                        if (isset($_SESSION['logedin']) && $_SESSION['logedin'] === true) { ?>
+
+                            <a href="view/userpage.php" class=" text-decoration-none"><i class="fa-regular fa-circle-user"style="font-size:1.5rem;"></i></a>
+
+                            <?php echo "Hi," . $_SESSION['username']; ?>
+                        <?php } else { ?>
+                            <button class="lo-button btn-sm ms-2 px-3">
+                                <a href="view/login.php" class=" text-decoration-none">login</a>
+                            </button>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
@@ -111,19 +113,14 @@
                 <div class="flex gap-3 my-auto">
                     <button class="p-1 bg-[black] text-white rounded-md px-2"><li class="fa-solid fa-magnifying-glass"""></li></button>
                     <button class="p-1 rounded-md px-2 bg-[#F6F6F6]"><li class="fa-solid fa-magnifying-glass"""></li></button>
-                </div>
-
-                
+                </div>      
             </div>
-
         </div>
 
     </div>
 
-
-
     <!--footer-->
-    <div class="container-fluid" style="background-color:burlywood;">
+    <div class="container-fluid footer">
         <div class="container p-3">
             <div class="row">
                 <div class="col">
@@ -145,27 +142,23 @@
                 </div>
                 <div class="col lin">
                     <h5>Connect with Us</h5>
-                    <p><a href=""><i class="fa-brands fa-facebook"
-                                style="font-size:50px;"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=""><i
-                                class="fa-brands fa-instagram"
-                                style="font-size:50px;"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=""><i
-                                class="fa-brands fa-youtube" style="font-size:50px;"></i></a></p>
+                    <p><a href=""><i class="fa-brands fa-facebook" style="font-size:50px;"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=""><i class="fa-brands fa-instagram" style="font-size:50px;"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=""><i class="fa-brands fa-youtube" style="font-size:50px;"></i></a></p>
                 </div>
             </div>
             <div class="row mt-2">
                 <div class="d-flex justify-content-between flex-column flex-md-row">
-                    <div><i class="fa-brands fa-cc-visa" style="font-size:50px;"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i
-                            class="fa-brands fa-cc-mastercard" style="font-size:50px;"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i
-                            class="fa-brands fa-cc-amex" style="font-size:50px;"></i></div>
+                    <div><i class="fa-brands fa-cc-visa" style="font-size:50px;"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-cc-mastercard" style="font-size:50px;"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-cc-amex" style="font-size:50px;"></i></div>
                     <div>&copy;Exchanze All Rights are reserved</div>
 
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="view/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
 </body>
+
 
 </html>
